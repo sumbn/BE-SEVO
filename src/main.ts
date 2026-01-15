6352c5f2f5b5ce457610dc5f9c1118ee57d79644
sum bùi <sumbn37k02.1@gmail.com>
2026-01-15 16:32:51 +0700

    update git ignore

--- src/main.ts
+++ src/main.ts
@@ -7,22 +7,24 @@ import { GlobalExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.setGlobalPrefix('api');
-  
+
   // Global Interceptors & Filters
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new GlobalExceptionFilter());
-  
+
   // Global Validation
-  app.useGlobalPipes(new ValidationPipe({ 
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
-  
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   app.use(cookieParser());
-  
+
   app.enableCors({
     origin: ['http://localhost:3000', 'http://localhost:3001'],
     credentials: true,
